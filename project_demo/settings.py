0709adf275806121ca2731a8d7455317707f4d4b
Worrasa <94108115+Worrasa@users.noreply.github.com>
2022-08-01 16:08:21 +0700

    demodld;dd

--- project_demo/settings.py
+++ project_demo/settings.py
@@ -25,7 +25,12 @@ SECRET_KEY = 'django-insecure-h&rr&-xlf0%w6sc*gvluhx8%gpo(s3-+-!%++f+n5j4i8@_qk0
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1','worrasak-projects.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1',
+    'worrasak-projects.herokuapp.com',
+    'worrasak-projects.herokuapp.com/',
+    'https://worrasak-projects.herokuapp.com/',
+    'https://worrasak-projects.herokuapp.com'
+]
 
 
 # Application definition
